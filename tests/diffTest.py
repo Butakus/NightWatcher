1b57452532f8a48931e559a0d8dab670112fc255
Butakus <butakus@gmail.com>
2015-09-12 23:41:20 +0200

    Video recording test added

--- tests/diffTest.py
+++ tests/diffTest.py
@@ -6,14 +6,14 @@ if __name__ == '__main__':
 	
 	if not ret:
 		print "Couldn't get the frame"
-		exit(0)
-	gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+		exit(1)
+	grey1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 
 	for _ in xrange(30): ret, frame1 = cap.read() # Drop some frames to get a different one	
 	ret, frame2 = cap.read()
-	gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+	grey2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-	diff = cv2.absdiff(gray1, gray2)
+	diff = cv2.absdiff(grey1, grey2)
 	# Blur the diff to smooth it. Need to select kernel size
 	diff = cv2.GaussianBlur(diff, (21,21), 0)
 	cv2.imwrite('../outputs/diffTest.png', diff)
--- tests/webcamTest.py
+++ tests/webcamTest.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
 	ret, frame = cap.read()
 	if not ret:
 		print "Couldn't get the frame"
-		exit(0)
+		exit(1)
 	cv2.imwrite('../outputs/imgTest.png', frame)
