dd10c155f66f32cf551501d3c7c0a0708d1ec3e1
Butakus <butakus@gmail.com>
2015-10-03 17:30:57 +0200

    FPS Meter test added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NightWatcher
-C++ Sleep monitor with OpenCV.
+Sleep monitor with C++ and OpenCV.
 
 Prepare your infra-red webcam and start recording yourself while sleeping. This software will detect the movement and make a video with only those moments to be analized later.
---
+++ tests/fps_meter.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Key codes returned by waitKey()
+#define KEY_q 1048689
+
+int main(int argc, char const *argv[]){
+
+	VideoCapture cap(0);
+	if (!cap.isOpened()){
+		cout << "Could not open the Webcam.\n";
+		return 0;
+	}
+
+	namedWindow("FPS Meter", WINDOW_AUTOSIZE);
+	Mat frame;
+
+	// Init
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	cap >> frame;
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	double frame_time = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+	double fps = 0.95 * fps + 0.05 * (1000.0 / frame_time);
+
+	cout << "Webcam FPS Meter.\n";
+	cout << "Press 'q' to quit.\n";
+	while (waitKey(1) != KEY_q){
+		t1 = chrono::high_resolution_clock::now();
+		cap >> frame;
+		t2 = chrono::high_resolution_clock::now();
+		imshow("FPS Meter", frame);
+		frame_time = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+		fps = 0.95 * fps + 0.05 * (1000.0 / frame_time);
+		cout << "FPS: " << fps << "        \r" << flush;
+	}
+	cout << "FPS: " << fps << "\n";
+	return 0;
+}
